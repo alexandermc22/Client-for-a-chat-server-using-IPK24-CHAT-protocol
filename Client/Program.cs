f70b546e4258dd459ba18c8c3ed6cb4410074a5b
alexandermc22 <sasha30092003@gmail.com>
2024-03-15 20:57:38 +0100

    cleaning code

--- Client/Program.cs
+++ Client/Program.cs
@@ -4,7 +4,6 @@ using CommandLine;
 using System.Net;
 using System.Net.Sockets;
 using System.Reflection;
-using System.Security.Cryptography.X509Certificates;
 
 namespace Client;
 
@@ -20,31 +19,14 @@ class Program
     }
     private static Mutex _mutexSate = new Mutex();
     private static State _state = State.START;
-    private static HistoryOfCommunication history = new HistoryOfCommunication();
     private static Queue<Confirm> ConfirmList = new Queue<Confirm>();
     private static Queue<Reply> ReplyList = new Queue<Reply>();
     private static Mutex mutexConfirm = new Mutex();
     private static Mutex mutexReply = new Mutex();
     private static Err _err;
     private static string? _displayName;
-    static async Task Main(string[] args)
+    static async Task<int> Main(string[] args)
     {
-        // Confirm c = new Confirm();
-        // c.MessageId = 0;
-        // Confirm c1 = new Confirm();
-        // c1.MessageId = 1;
-        // Confirm c2 = new Confirm();
-        // c1.MessageId = 2;
-        // Reply r = new Reply();
-        // r.MessageId = 1;
-        // r.RefMessageId = 0;
-        // r.Result = true;
-        // r.MessageContent="ok";
-        // ConfirmList.Enqueue(c);
-        // ConfirmList.Enqueue(c1);
-        // ConfirmList.Enqueue(c2);
-        // ReplyList.Enqueue(r);
-        
         var parser = new Parser(with => with.EnableDashDash = false);
         var result = parser.ParseArguments<Options>(args)
             .WithParsed(async options =>
@@ -57,27 +39,21 @@ class Program
 
                 if (options.Protocol == "" || options.Ip == "")
                 {
-                    Console.WriteLine("Error parsing arguments");
+                    Console.WriteLine("ERR: error parsing arguments");
                     Console.WriteLine("Use -h for help");
                     return;
                 }
-                //HistoryOfCommunication history = new HistoryOfCommunication();
                 
                 IPAddress[] addresses = Dns.GetHostAddresses(options.Ip);
 
                 if (addresses.Length == 0)
                 {
-                    Console.WriteLine("Wrong IP");
+                    Console.WriteLine("ERR: Wrong IP");
                     return;
                 }
                 
                 IPAddress ip = addresses[0];
                 
-                // if (!IPAddress.TryParse(options.Ip, out IPAddress? ip))
-                // {
-                //     Console.WriteLine("Wrong IP");
-                //     return;
-                // }
 
                 if (options.Protocol.Equals("tcp", StringComparison.OrdinalIgnoreCase))
                 {
@@ -85,12 +61,11 @@ class Program
                     {
                         try
                         {
-                            // Подключаемся к серверу
                             tcpClient.Connect(ip, options.Port);
                         }
                         catch (Exception ex)
                         {
-                            Console.WriteLine($"Ошибка подключения: {ex.Message}");
+                            Console.WriteLine($"ERR: {ex.Message}");
                         }
                     }
                 }
@@ -100,31 +75,28 @@ class Program
                     {
                             try
                             {
-                                await UdpProcessSocketCommunication(options, history,ip);
+                                await UdpProcessSocketCommunication(options,ip);
                             }
                             catch (Exception ex)
                             {
-                                Console.WriteLine($"wtf: {ex.Message}");
+                                Console.Error.WriteLine($"ERR: {ex.Message}");
                             }
+                            return;
                     }
                     else
                     {
-                        Console.WriteLine("Wrong protocol");
+                        Console.WriteLine("ERR: Wrong protocol");
                         return;
                     }
                 }
             })
             .WithNotParsed(errors =>
             {
-                foreach (var error in errors)
-                {
-                    Console.WriteLine($"error: {error}");
-                }
-
                 Console.WriteLine("Error parsing arguments");
                 Console.WriteLine("Use -h for help");
                 return;
             });
+        return 0;
     }
 
     static async Task<int> TcpProcessSocketCommunication(
@@ -139,25 +111,18 @@ class Program
         return 0;
     }
 
-    static async Task<int> UdpProcessSocketCommunication(Options options, HistoryOfCommunication history,IPAddress ip)
+    static async Task<int> UdpProcessSocketCommunication(Options options,IPAddress ip)
     {
         IMessage.MessageId = 0;
-            //UdpClient udpClientReceive = new UdpClient();
         UdpClient udpClientSend = new UdpClient();
         IPEndPoint serverEP = new IPEndPoint(ip, options.Port);
         IPEndPoint clientEP = new IPEndPoint(IPAddress.Any, 0);
         udpClientSend.Client.Bind(clientEP);
         
         Task<int> receiveTask = ReceiveMessage(udpClientSend,serverEP,udpClientSend,options,ip,clientEP);
-        Task<int> readTask = ReadMessage(history, options,udpClientSend,serverEP);
+        Task<int> readTask = ReadMessage(options,udpClientSend,serverEP);
         Task result = await Task.WhenAny(readTask, receiveTask);
         udpClientSend.Close();
-        if(result==receiveTask)
-            Console.WriteLine("rec");
-        else
-        {
-            Console.WriteLine("read");
-        }
         return 0;
     }
 
@@ -168,8 +133,6 @@ class Program
         {
             while (true)
             {
-                // if (_state == State.END)
-                //     return 0;
                 UdpReceiveResult receiveResult = await udpClient.ReceiveAsync();
                 _mutexSate.WaitOne();
                 switch (_state)
@@ -181,7 +144,6 @@ class Program
                         _mutexSate.ReleaseMutex();
                         if (receiveResult.RemoteEndPoint.Port != serverEP.Port)
                         {
-                            //clientEP.Port = receiveResult.RemoteEndPoint.Port;
                             serverEP.Port = receiveResult.RemoteEndPoint.Port;
                         }
                 
@@ -202,7 +164,7 @@ class Program
                             case (byte)MessageType.ERR:
                                 a = SendConfirm(receiveResult.Buffer, udpClient, serverEP);
                                 Err err = Err.FromBytes(receiveResult.Buffer);
-                                Console.Error.WriteLine($"ERROR FROM {err.DisplayName}: {err.MessageContents}");
+                                Console.Error.WriteLine($"ERR FROM {err.DisplayName}: {err.MessageContents}");
 
                                 Task close = SendBye(sender, serverEP, options);
                                 Task<int> readConfirm = ReceiveMessage(udpClient,serverEP,sender,options,ip,clientEP);
@@ -233,7 +195,7 @@ class Program
                             case (byte)MessageType.ERR:
                                 a = SendConfirm(receiveResult.Buffer, udpClient, serverEP);
                                 Err err = Err.FromBytes(receiveResult.Buffer);
-                                Console.Error.WriteLine($"ERROR FROM {err.DisplayName}: {err.MessageContents}");
+                                Console.Error.WriteLine($"ERR FROM {err.DisplayName}: {err.MessageContents}");
                                 
                                 Task close = SendBye(sender, serverEP, options);
                                 Task<int> readConfirm = ReceiveMessage(udpClient,serverEP,sender,options,ip,clientEP);
@@ -258,7 +220,6 @@ class Program
                                 Task<int> readConfirm2 = ReceiveMessage(udpClient,serverEP,sender,options,ip,clientEP);
                                 await sendError;
                                 Task close1 = SendBye(sender, serverEP, options);
-                                //Task<int> readConfirm1 = ReceiveMessage(udpClient,serverEP,sender,options);
                                 await close1;
                                 return 0;
                         }
@@ -276,15 +237,15 @@ class Program
                 }
             }
         }
-        catch (ArgumentException) // сделать если на вход говно
+        catch (ArgumentException) 
         {
-            Console.WriteLine("end");
+            Console.WriteLine("ERR: ArgumentException");
             throw ;
         }
 
     }
 
-    static async Task<int> ReadMessage(HistoryOfCommunication history, Options options,UdpClient udpClient,IPEndPoint localEndPoint)
+    static async Task<int> ReadMessage(Options options,UdpClient udpClient,IPEndPoint localEndPoint)
     {
         
         while (true)
@@ -301,6 +262,8 @@ class Program
                 {
                     _mutexSate.ReleaseMutex();
                 }
+                Console.WriteLine("End of communication");
+
                 Environment.Exit(0);
             };
             string? userInput = Console.ReadLine();
@@ -311,7 +274,7 @@ class Program
 
             if (string.IsNullOrWhiteSpace(userInput))
             {
-                Console.Error.WriteLine("ERROR: Wrong input, repeat");
+                Console.Error.WriteLine("ERR: Wrong input, repeat");
                 continue;
             }
 
@@ -319,7 +282,7 @@ class Program
 
             if (words.Length == 0)
             {
-                Console.Error.WriteLine("ERROR: Wrong input, repeat");
+                Console.Error.WriteLine("ERR: Wrong input, repeat");
                 continue;
             }
 
@@ -341,7 +304,7 @@ class Program
                         case "/auth":
                             if (words.Length != 4)
                             {
-                                Console.Error.WriteLine("ERROR: Wrong input, repeat");
+                                Console.Error.WriteLine("ERR: Wrong input, repeat");
                                 continue;
                             }
 
@@ -356,7 +319,7 @@ class Program
                             _state = State.AUTH;
                             _mutexSate.ReleaseMutex();
                             result = await SendMessageAsync(auth.ToBytes(), udpClient, localEndPoint, options);
-                            if (result == 0)
+                            if (result == 1)
                             {
                                 int replyBool = await WaitReply(IMessage.MessageId , options);
                                 if (replyBool == 1)
@@ -367,7 +330,6 @@ class Program
                                 }
                                 else if (replyBool == -1)
                                 {
-                                    Console.Error.WriteLine("ERROR: no reply");
                                     await SendBye(udpClient, localEndPoint, options);
                                     return 1;
                                 }
@@ -376,12 +338,11 @@ class Program
                             }
                             else
                             {
-                                Console.Error.WriteLine("ERROR: No confirm");
                                 await SendBye(udpClient, localEndPoint, options);
                                 return 1;
                             }
                         default:
-                            Console.Error.WriteLine("ERROR: Wrong input, repeat");
+                            Console.Error.WriteLine("ERR: Wrong input, repeat");
                             continue;
                     }
                 case State.OPEN:
@@ -391,7 +352,7 @@ class Program
                         case "/join":
                             if (words.Length != 2)
                             {
-                                Console.Error.WriteLine("ERROR: Wrong input, repeat");
+                                Console.Error.WriteLine("ERR: Wrong input, repeat");
                                 continue;
                             }
 
@@ -401,12 +362,11 @@ class Program
                                 DisplayName = _displayName
                             };
                             result = await SendMessageAsync(join.ToBytes(), udpClient, localEndPoint, options);
-                            if (result == 0)
+                            if (result == 1)
                             {
                                 int replyBool = await WaitReply(IMessage.MessageId , options);
                                 if (replyBool == -1)
                                 {
-                                    Console.Error.WriteLine("ERROR: no reply");
                                     await SendBye(udpClient, localEndPoint, options);
                                     return 1;
                                 }
@@ -414,14 +374,13 @@ class Program
                             }
                             else
                             {
-                                Console.Error.WriteLine("ERROR: No confirm");
                                 await SendBye(udpClient, localEndPoint, options);
                                 return 1;
                             }
                         case "/rename":
                             if (words.Length != 2)
                             {
-                                Console.Error.WriteLine("ERROR: Wrong input, repeat");
+                                Console.Error.WriteLine("ERR: Wrong input, repeat");
                                 continue;
                             }
                             _displayName = words[1];
@@ -430,7 +389,7 @@ class Program
                             //TODO: проверить состояние отправить сообщение на сервер, проверить 1 символо на / и вывести ошибку
                             if (words[0][0] == '/')
                             {
-                                Console.Error.WriteLine("ERROR: Wrong input, repeat");
+                                Console.Error.WriteLine("ERR: Wrong input, repeat");
                                 continue;
                             }
 
@@ -441,11 +400,10 @@ class Program
                             };
 
                             result = await SendMessageAsync(msg.ToBytes(), udpClient, localEndPoint, options);
-                            if (result == 0)
+                            if (result == 1)
                                 continue;
                             else
                             {
-                                Console.Error.WriteLine("ERROR: No confirm");
                                 await SendBye(udpClient, localEndPoint, options);
                                 return 1;
                             }
@@ -478,14 +436,15 @@ class Program
                 {
                     mutexConfirm.ReleaseMutex();
                     IMessage.MessageId++;
-                    return 0;
+                    return 1;
                 }
             }
 
             mutexConfirm.ReleaseMutex();
         }
         IMessage.MessageId++;
-        return 1;
+        Console.Error.WriteLine("ERR: No confirm");
+        return 0;
     }
 
     static async Task SendConfirm(byte[] message, UdpClient udpClient, IPEndPoint localEndPoint)
@@ -527,7 +486,7 @@ class Program
 
             mutexConfirm.ReleaseMutex();
         }
-
+        Console.Error.WriteLine("ERR: no reply");
         return -1; //err
     }
 }
